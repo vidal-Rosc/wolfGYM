2de0ff3c5587065e13082fbb5285885f75b6186d
vidal-Rosc <vidal_jose84@hotmail.com>
2020-04-29 14:26:03 +0200

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,9 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 import { AngularFireAuth } from '@angular/fire/auth';
 import { NgxSpinnerService } from 'ngx-spinner';
 
+
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -27,16 +30,15 @@ export class LoginComponent implements OnInit {
     //    })
     this.loginForm = this.creadorFormulario.group({
       email: ['', Validators.compose([
-              Validators.required, Validators.email
+        Validators.required, Validators.email
       ])],
       password: ['', Validators.compose([
-              Validators.required, Validators.minLength(6)
+        Validators.required, Validators.minLength(6)
       ])],
     })
 
   }
-
-
+//login
   acceder() {
     if (this.loginForm.valid) {
       this.datosCorrectos = true;
@@ -45,7 +47,7 @@ export class LoginComponent implements OnInit {
         .then((usuario) => {
           console.log(usuario)
           this.spinner.hide();
-        }).catch((error)=>{
+        }).catch((error) => {
           this.datosCorrectos = false;
           this.mensajeError = error.message;
           this.spinner.hide();
