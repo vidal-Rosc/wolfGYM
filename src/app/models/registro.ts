561bee25521b6a1bd6e96bcbd85300e7398321a0
vidal-Rosc <vidal_jose84@hotmail.com>
2020-05-02 16:35:36 +0200

    realizando la Inscripcion

--- src/app/models/registro.ts
+++ src/app/models/registro.ts
@@ -5,17 +5,63 @@ export class Registro {
   fechaFinal: Date;
   cliente: DocumentReference;
   precios: DocumentReference;
-  subtotal: number;
+  subTotal: number;
   iva: number;
   total: number;
 
-  constructor(){
+  constructor() {
     this.fecha = null;
-    this.fechaFinal = this.fechaFinal;
+    this.fechaFinal = null;
     this.cliente = this.cliente;
     this.precios = this.precios;
-    this.subtotal = this.subtotal;
+    this.subTotal = this.subTotal;
     this.iva = this.iva;
     this.total = this.total;
   }
+
+  validar(): any {
+    let respuesta = {
+      esValido: false,
+      mensaje: '',
+    }
+
+    if (this.fecha == null || this.fecha == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No tiene fecha de inicio.';
+      return respuesta;
+    }
+    if (this.fechaFinal == null || this.fechaFinal == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No tiene fecha de  finalizacion.';
+      return respuesta;
+    }
+    if (this.cliente == null || this.cliente == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No se ha seleccionado un cliente';
+      return respuesta;
+    }
+    if (this.precios == null || this.precios == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No muestra los precios';
+      return respuesta;
+    }
+    if (this.subTotal <= 0 || this.subTotal == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No se refleja un subtotal.';
+      return respuesta;
+    }
+      if (this.iva <= 0 || this.iva == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No muestra el IVA';
+      return respuesta;
+    }
+    if (this.total <= 0  || this.total == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No se muestra la cantidad total.';
+      return respuesta;
+    }
+    respuesta.esValido = true;
+    return respuesta;
+  }
+
 }
--- src/app/registros/registros.component.ts
+++ src/app/registros/registros.component.ts
@@ -14,13 +14,13 @@ export class RegistrosComponent implements OnInit {
   registro: Registro = new Registro();
   ClienteSelected: Cliente = new Cliente();
   precios: Precio[] = new Array<Precio>();
-  precioSeleccionado: Precio =  new Precio();
+  precioSeleccionado: Precio = new Precio();
 
   constructor(private bbdd: AngularFirestore) { }
 
-  ngOnInit(){
-    this.bbdd.collection('precios').get().subscribe((resultado)=>{
-      resultado.docs.forEach((item)=>{
+  ngOnInit() {
+    this.bbdd.collection('precios').get().subscribe((resultado) => {
+      resultado.docs.forEach((item) => {
         let precio = item.data() as Precio;
         precio.id = item.id;
         precio.ref = item.ref;
@@ -29,57 +29,79 @@ export class RegistrosComponent implements OnInit {
     })
   }
 
-  asignarCliente(cliente:Cliente){
+  asignarCliente(cliente: Cliente) {
     this.registro.cliente = cliente.ref;
     this.ClienteSelected = cliente;
   }
 
-  cancelarCliente(){
+  cancelarCliente() {
     //de esta forma eliminamos los clientes de nuestro registro
     this.ClienteSelected = new Cliente();
     this.registro.cliente = undefined;
   }
 
-  guardar(){
+  guardar() {
     console.log(this.registro);
-  }
-
-  seleccionarPrecio(id:string){
-    console.log(id)
-    this.precioSeleccionado = this.precios.find( x => x.id == id);
-    this.registro.precios = this.precioSeleccionado.ref;
-    this.registro.fecha = new Date();
-
-
-    //Calcuando los dias
-    if(this.precioSeleccionado.plan == 1){
-      let dias = this.precioSeleccionado.duracion;
-      let fechaFinal =  new Date(this.registro.fecha.getFullYear(), this.registro.fecha.getMonth(), this.registro.fecha.getDate() + dias)
-      this.registro.fechaFinal = fechaFinal;
-      //fechaFinal = this.precioSeleccionado.duracion * 1;
-    }
-    if(this.precioSeleccionado.plan == 2){
-      let dias = this.precioSeleccionado.duracion * 7;
-      let fechaFinal =  new Date(this.registro.fecha.getFullYear(),this.registro.fecha.getMonth(),this.registro.fecha.getDate() + dias)
-      this.registro.fechaFinal = fechaFinal;
-
-    }
-    if(this.precioSeleccionado.plan == 3){
-      let dias = this.precioSeleccionado.duracion * 30;
-      let fechaFinal =  new Date(this.registro.fecha.getFullYear(),this.registro.fecha.getMonth(),this.registro.fecha.getDate() + dias)
-      this.registro.fechaFinal = fechaFinal;
-    }
-    if(this.precioSeleccionado.plan == 4){
-      let meses = this.precioSeleccionado.duracion * 3;
-      let fechaFinal =  new Date(this.registro.fecha.getFullYear(),this.registro.fecha.getMonth() + meses,this.registro.fecha.getDate())
-      this.registro.fechaFinal = fechaFinal;
-    }
-    if(this.precioSeleccionado.plan == 5){
-      let anio = this.registro.fecha.getFullYear();
-      let meses = this.precioSeleccionado.duracion * 12 + this.registro.fecha.getMonth();
-      let dia: number = this.registro.fecha.getDate();
-      let fechaFinal =  new Date(anio,meses,dia)
-      this.registro.fechaFinal = fechaFinal;
+    if(this.registro.validar().esValido){
+      console.log('Guardando....')
+    } else {
+      console.log(this.registro.validar().mensaje)
     }
   }
+
+  seleccionarPrecio(id: string) {
+
+    if (id != null) {
+      console.log(id)
+      this.precioSeleccionado = this.precios.find(x => x.id == id);
+      //this.registro.precios = this.precioSeleccionado.ref
+      this.registro.fecha = new Date();
+
+
+      //Calculando los precios:
+      this.registro.subTotal = this.precioSeleccionado.costo;
+      this.registro.iva = this.registro.subTotal * 0.21;
+      this.registro.total = this.registro.subTotal + this.registro.iva;
+
+      //Calcuando los dias
+      if (this.precioSeleccionado.plan == 1) {
+        let dias = this.precioSeleccionado.duracion;
+        let fechaFinal = new Date(this.registro.fecha.getFullYear(), this.registro.fecha.getMonth(), this.registro.fecha.getDate() + dias)
+        this.registro.fechaFinal = fechaFinal;
+        //fechaFinal = this.precioSeleccionado.duracion * 1;
+      }
+      if (this.precioSeleccionado.plan == 2) {
+        let dias = this.precioSeleccionado.duracion * 7;
+        let fechaFinal = new Date(this.registro.fecha.getFullYear(), this.registro.fecha.getMonth(), this.registro.fecha.getDate() + dias)
+        this.registro.fechaFinal = fechaFinal;
+
+      }
+      if (this.precioSeleccionado.plan == 3) {
+        let dias = this.precioSeleccionado.duracion * 30;
+        let fechaFinal = new Date(this.registro.fecha.getFullYear(), this.registro.fecha.getMonth(), this.registro.fecha.getDate() + dias)
+        this.registro.fechaFinal = fechaFinal;
+      }
+      if (this.precioSeleccionado.plan == 4) {
+        let meses = this.precioSeleccionado.duracion * 3;
+        let fechaFinal = new Date(this.registro.fecha.getFullYear(), this.registro.fecha.getMonth() + meses, this.registro.fecha.getDate())
+        this.registro.fechaFinal = fechaFinal;
+      }
+      if (this.precioSeleccionado.plan == 5) {
+        let anio = this.registro.fecha.getFullYear();
+        let meses = this.precioSeleccionado.duracion * 12 + this.registro.fecha.getMonth();
+        let dia: number = this.registro.fecha.getDate();
+        let fechaFinal = new Date(anio, meses, dia)
+        this.registro.fechaFinal = fechaFinal;
+      }
+    } else {
+      this.precioSeleccionado = new Precio();
+      this.registro.fecha = null;
+      this.registro.fechaFinal = null;
+      this.registro.precios = null;
+      this.registro.subTotal = 0;
+      this.registro.iva = 0;
+      this.registro.total = 0;
+    }
+  }
+
 }
